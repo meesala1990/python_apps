aab5e777ecde93ea43782463237950ec88e93536
meesala1990 <mesalasandeep@gmail.co>
2022-04-26 18:10:24 -0400

    jira changes

--- demo.py
+++ demo.py
@@ -1,1 +1,4 @@
-print ('hello')
+from jira import JIRA 
+jiraOptions = {'server':'https://jira.gfs.com/'}
+jira = JIRA(options = jiraOptions, basic_auth = ("sandeep","Samsung@0790"))
+for singleIssue in jira.search_issues(jql_str='project in (TS, DUS, NATC) AND status = "Ready for Release"'):print('{}: {}:{}'.format(singleIssue.key, singleIssue.fields.summary,singleIssue.fields.reporter.displayName))
